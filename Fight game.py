5d9c8cde9ddcd8b16863dbe70dbe196973763810
Pszemek <kojot137@gmail.com>
2021-01-20 18:06:59 +0100

    added opponent responses

--- Fight game.py
+++ Fight game.py
@@ -1,9 +1,12 @@
+import random
+
 def fighting_game():
     print("Welcome in my extraordinary fighting game !\nType help for information")
     logic = True
     fighting = False
     blocking = False
     command = ""
+    opponent_last_move = 0
     while logic:
         command = input("> ")
         if command == "help":
@@ -11,7 +14,7 @@ def fighting_game():
     start - to start a fight
     block - to start blocking
     stop - to stop a fight
-    quit - to quit fight 
+    quit - to quit fight
             """)
         elif command == "start":
             if fighting:
@@ -19,9 +22,13 @@ def fighting_game():
             else:
                 fighting = True
                 print("You started throwing punches !")
+                if(opponent_last_move == 2):
+                    print("But he blocked you!!")
         elif command == "block":
             if fighting:
                 print("You cannot throwing punches, and block at the same time")
+                if(opponent_last_move == 1):
+                    print("You blocked opponent")
             elif blocking:
                 print("You already have your guard up !")
             elif not fighting:
@@ -39,5 +46,13 @@ def fighting_game():
         else:
             print("I don't understand this command :<")
 
+        rand_num = random.choice(range(1,3))
+        opponent_last_move = rand_num
+        if(rand_num == 1):
+            print("opponent attacked")
+        elif(rand_num == 2):
+            print("opponent blocked")
+        else:
+            print("opponent run away")
 
 fighting_game()
